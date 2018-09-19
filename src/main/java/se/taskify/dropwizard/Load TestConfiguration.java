51c79d9837bf6bbeede49596571833d31d0cba2b
PhroZen <phrozen@teamsamst.com>
2018-09-20 00:04:03 +0200

    Initial commit

---
+++ src/main/java/se/taskify/dropwizard/Load TestConfiguration.java
@@ -0,0 +1,10 @@
+package se.taskify.dropwizard;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class Load TestConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
